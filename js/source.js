5fe2808ff44d8d6de22b666e3c5590c09130e911
liconeto <benicio.neto9@gmail.com>
2023-04-14 17:57:18 -0300

    First commit starting the project

---
+++ js/source.js
@@ -0,0 +1,24 @@
+import fetch from "node-fetch";
+globalThis.fetch = fetch;
+const md5 = require("md5");
+
+const url = "https://gateway.marvel.com:443/v1/public/characters?";
+const publicKey = "15028edce7d7047ff114a2da91a75a0f";
+const privateKey = "9379705b51c6aeb6302f4b4db5243495bf544867";
+let dataApi = {};
+
+getAnswers = async () => {
+  //Gerando Time Stamp
+  let ts = Number(new Date());
+  //Gerando hash md5 (timeStamp + chavePrivada + chavePublica)
+  let hash = md5(ts + privateKey + publicKey);
+
+  let dataQuestionsUn = await fetch(
+    `${url}&ts=${ts}&apikey=${publicKey}&hash=${hash}`
+  ).then((res) => res.json());
+
+  console.log(`${url}&ts=${ts}&apikey=${publicKey}&hash=${hash}`);
+  dataApi = dataQuestionsUn;
+};
+
+console.log(getAnswers());
